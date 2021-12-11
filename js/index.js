1a6625b288317f101f8dd73321680a2834ea9bc6
root <root@lazyCoder.shahed>
2021-12-12 00:27:12 +0600

    add silk

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: false,
+    autoplaySpeed: 1000,
+    centerMode: true,
+    centerPadding: 0
+  });
